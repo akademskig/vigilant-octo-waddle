fcedb877b32a7762db362f7d29f3405ab208c9c5
Marta Sušek <m.susek@live.com>
2020-05-05 20:59:37 +0200

    Add a monkey.

--- src/pages/registration.page.tsx
+++ src/pages/registration.page.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import FRegistration from './components/forms/FRegistration';
 import styled from 'styled-components';
+import monkeyImg from '../assets/monkey.svg'
 
 const RegistrationPageS = styled.div`
 justify-content: center;
@@ -8,11 +9,14 @@ height: 100vh;
 align-items: center;
 padding-top: 20vh;
 box-sizing: border-box;
-background-color: ${({theme}) => theme.colors.primary.dark};
+background-color: ${({ theme }) => theme.colors.primary.dark};
+background-image: url(${monkeyImg});
+background-repeat: no-repeat;
+background-position-x: -30px;
 `
-function RegistrationPage(){
+function RegistrationPage() {
 
-    return(
+    return (
         <RegistrationPageS>
             <FRegistration></FRegistration>
         </RegistrationPageS>
