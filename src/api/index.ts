12b4da786cfcb5592ad22b7be6d6fedc694a5931
Marta Sušek <m.susek@live.com>
2020-05-07 21:21:37 +0200

    Modify mock api.

--- src/api/index.ts
+++ src/api/index.ts
@@ -7,17 +7,24 @@ const existingData = [
         valueStr: 'Marta Susek'
     }
 ]
-export function registerUser(data: any) {
-    let username = data.fields.find((d: any) => d.code === "username")
-    let existing = existingData.find(ed => ed.code === username.code && ed.valueStr === username.valueStr)
-    let error = existing
+
+export function registerUser(form: any) {
+    const body = parseData(form)
+    let uniqueField = body.fields.find((d: any) => d.code === "username")
+    let existing = existingData.find(ed => ed.code === uniqueField.code && ed.valueStr === uniqueField.valueStr)
+    let error: any = null
+    if (existing) {
+        const fieldName = form.find((field: any) => field.code === existing?.code).name
+        error = `${fieldName} ${existing?.valueStr} already exists!`
+    }
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             if (error) {
                 reject({
                     info: {
                         success: false,
-                        message: "Request failed!"
+                        message: "Request failed!",
+                        error: error
                     }
                 })
             } else {
@@ -30,4 +37,14 @@ export function registerUser(data: any) {
             }
         }, 3000)
     })
+}
+
+function parseData(form: any) {
+    const formData = form.map(({ code, value, dataType }: { code: string, value: string, dataType: string }) => ({
+        code: code,
+        valueStr: value,
+        dataType: dataType
+    }))
+    return { fields: formData }
+
 }
