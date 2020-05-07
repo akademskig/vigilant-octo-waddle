5f28ca973ff35dee42d9d8eb3cdca0460fdf4b27
Marta Sušek <m.susek@live.com>
2020-05-07 21:26:49 +0200

    Modify background, add alt prop.

--- src/pages/registration.page.tsx
+++ src/pages/registration.page.tsx
@@ -6,23 +6,28 @@ import monkeyFormImg from '../assets/monkeyForm.svg'
 
 const RegistrationPageS = styled.div`
 justify-content: center;
-height: 100vh;
+min-height: 100vh;
 align-items: center;
 padding-top: 5vh;
+padding-bottom: 5vh;
 box-sizing: border-box;
 background-color: ${({ theme }) => theme.colors.primary.dark};
 background-image: url(${monkeyImg});
 background-repeat: no-repeat;
-background-position-x: -30px;
+background-position-x: right;
+background-position-y: bottom;
+background-size: 250px;
 .monkey-img{
     width: 8em;
+    display: block;
+    margin:auto;
 }
 `
 function RegistrationPage() {
 
     return (
         <RegistrationPageS>
-            <img className="monkey-img"src={monkeyFormImg}></img>
+            <img className="monkey-img"src={monkeyFormImg} alt="Monkey"></img>
             <FRegistration></FRegistration>
         </RegistrationPageS>
     )
