d58a90b0e00162075a79d35598208bbdcc364791
Marta Sušek <m.susek@live.com>
2020-05-09 14:29:01 +0200

    Minor regex fix.

--- src/pages/components/forms/formData.ts
+++ src/pages/components/forms/formData.ts
@@ -123,5 +123,5 @@ export const form = [
         }
     },
 ]
-export const strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})")
+export const strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#%&])(?=.{8,})")
 export const mediumRegex = new RegExp("^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})");
